24c170f144305fe198c0669b8b0666a1e26b9247
Toma-123 <casa1406@naver.com>
2022-11-01 16:23:06 +0900

    Rain Wan
    basil(prev) <- backup

--- js/razer_basil.js
+++ js/razer_basil.js
@@ -26,20 +26,20 @@
         const upWord = document.querySelectorAll('.upword')
         const upWord2 = document.querySelectorAll('.upword2')
     
-        upWord.forEach((ele)=>{
-            offset.push(ele.offsetTop - window.innerHeight*0.7)
-        });
+        // upWord.forEach((ele)=>{
+        //     offset.push(ele.offsetTop - window.innerHeight*0.8)
+        // });
     
-        upWord2.forEach((ele)=>{
-            offset2.push(ele.offsetTop - window.innerHeight*0.7)
-        })
+        // upWord2.forEach((ele)=>{
+        //     offset2.push(ele.offsetTop - window.innerHeight*0.8)
+        // })
         
-        console.log(offset);
-        console.log(offset2);
+        // console.log(offset);
+        // console.log(offset2);
     
     
         window.addEventListener('scroll',()=>{
-            console.log(window.pageYOffset);
+            // console.log(window.pageYOffset);
             sd.y = window.pageYOffset;
     
             if(sd.y > sd.y2){
@@ -55,6 +55,7 @@
             if(sd.state){
                 //내려갈때(true)
                 upWord.forEach((ele,key)=>{
+                    offset.push(ele.offsetTop - window.innerHeight*0.8)
                     if(window.pageYOffset > offset[key]){
                         upWord[key].classList.add('active');
                     }
@@ -62,6 +63,7 @@
                 })
     
                 upWord2.forEach((ele,key)=>{
+                    offset2.push(ele.offsetTop - window.innerHeight*0.8)
                     if(window.pageYOffset > offset2[key]){
                         upWord2[key].classList.add('active')
                     }
@@ -69,18 +71,69 @@
     
             }else{
                 upWord.forEach((ele,key)=>{
+                    
                     if(window.pageYOffset < offset[key]){
                         upWord[key].classList.remove('active');
                     }
                 })
     
                 upWord2.forEach((ele,key)=>{
+                    
                     if(window.pageYOffset > offset2[key]){
                         upWord2[key].classList.remove('active')
                     }
                 })
             }
         });
+
+
+        const rainContainer = document.querySelector(".rain-container");
+
+            // background Colors for the raindrop
+            const background = [
+                "linear-gradient(transparent, skyblue)",
+                "linear-gradient(transparent, red)",
+                "linear-gradient(transparent, green)",
+                "linear-gradient(transparent, white)",
+                "linear-gradient(transparent, yellow)",
+                "linear-gradient(transparent, blue)",
+                "linear-gradient(transparent, purple)",
+                "linear-gradient(transparent, pink)"
+            ];
+
+            // red , yellow, green, purple, blue, navy
+
+            const amount = 400; // amount of raindops
+            let i = 0;
+
+            // Looping and creating the raindrop then adding to the rainContainer
+            while (i < amount) {
+                //  Creating and Element
+                const drop = document.createElement("i");
+
+                //   CSS Properties for raindrop
+                const raindropProperties = {
+                    width: Math.random() * 3 + "px",
+                    positionX: Math.floor(Math.random() * window.innerWidth) + "px",
+                    delay: Math.random() *  -5 + "s",
+                    duration: (Math.random()) + 2 + "s",
+                    bg: background[Math.round(Math.random() * background.length)],
+                    opacity: Math.random() + 10
+                };
+
+                //   Setting Styles for raindrop
+                drop.style.width = raindropProperties.width;
+                drop.style.left = raindropProperties.positionX;
+                drop.style.animationDelay = raindropProperties.delay;
+                drop.style.animationDuration = raindropProperties.duration;
+                drop.style.background = raindropProperties.bg;
+                drop.style.opacity = raindropProperties.opacity;
+
+                //   Appending the raindrop in the raindrop container
+                rainContainer.appendChild(drop);
+                i++;
+            }
+            
     }
 
     window.addEventListener('load',init);
--- js/razer_store.js
+++ js/razer_store.js
@@ -1,46 +1,98 @@
-const rightBtn = document.querySelector('.right_button');
-const leftBtn = document.querySelector('.left_button');
+
 const elSlide = document.querySelectorAll('.slide_ul');
+
+const rightBtn0 = document.querySelector('.right_button');
+const leftBtn0 = document.querySelector('.left_button');
+
+const leftBtn1 = document.querySelector('.left_button1')
+const rightBtn1 = document.querySelector('.right_button1')
 
 const leftBtn2 = document.querySelector('.left_button2')
 const rightBtn2 = document.querySelector('.right_button2')
 
-const leftBtn3 = document.querySelector('.left_button3')
-const rightBtn3 = document.querySelector('.right_button3')
-
         
 function scrollMove(i,t){
+    elSlide[i].scrollBy({top:0, left:t, behavior:"smooth"});  
+};
 
-            
-    elSlide[i].scrollBy({top:0, left:t, behavior:"smooth"});            
-}
 
-rightBtn.addEventListener('click',()=>{
+rightBtn0.addEventListener('click',()=>{
 
     scrollMove(0,1000);
+    
+
 })
 
-leftBtn.addEventListener('click',()=>{
+leftBtn0.addEventListener('click',()=>{    
     
     scrollMove(0,-1000);
+    
+
 })
 
 
-rightBtn2.addEventListener('click',()=>{
+elSlide[0].addEventListener('scroll',()=>{
+    
+    if(elSlide[0].scrollLeft > 1471){
+        rightBtn0.style.display="none"    
+    }else{
+        rightBtn0.style.display="block"
+    }
+    
+    if(elSlide[0].scrollLeft < 472 ){
+        leftBtn0.style.display="none"
+    }else{
+        leftBtn0.style.display="block"
+    }
+})
+
+elSlide[1].addEventListener('scroll',()=>{
+
+    if(elSlide[1].scrollLeft > 1471){
+        rightBtn1.style.display="none"    
+    }else{
+        rightBtn1.style.display="block"
+    }
+    
+    if(elSlide[1].scrollLeft < 471){
+        leftBtn1.style.display="none"
+    }else{
+        leftBtn1.style.display="block"
+    }
+})
+
+elSlide[2].addEventListener('scroll',()=>{
+
+    if(elSlide[2].scrollLeft > 1471){
+        rightBtn2.style.display="none"    
+    }else{
+        rightBtn2.style.display="block"
+    }
+    
+    if(elSlide[2].scrollLeft < 471){
+        leftBtn2.style.display="none"
+    }else{
+        leftBtn2.style.display="block"
+    }
+})
+
+
+
+rightBtn1.addEventListener('click',()=>{
     
     scrollMove(1,1000);
 })
 
-leftBtn2.addEventListener('click',()=>{
+leftBtn1.addEventListener('click',()=>{
     scrollMove(1,-1000);
 })
 
-rightBtn3.addEventListener('click',()=>{
+rightBtn2.addEventListener('click',()=>{
     
     scrollMove(2,1000);
 })
 
-leftBtn3.addEventListener('click',()=>{
+leftBtn2.addEventListener('click',()=>{
     scrollMove(2,-1000);
 })
 
